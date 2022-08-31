8b71c68a9908e756bac9b131a6a82fcd05c6cb41
a <a>
2022-08-31 11:22:32 +0800

    Ring Rose

---
+++ RingRose/js/RingRose.js
@@ -0,0 +1,196 @@
+/***
+ * 角度转弧度。
+ * @param deg {number} 输入角度。
+ * @returns {number} 输出弧度。
+ * @constructor
+ */
+function Rad(deg) {
+    return deg * Math.PI / 180;
+}
+
+class RingRoseDrawer {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.context = this.canvas.getContext("2d");
+        this.location = {x: canvas.width / 2, y: canvas.height / 2};
+        this.ringWidth = canvas.width < canvas.height ? canvas.width * 0.3 : canvas.height * 0.3;
+        this.insideRadius = canvas.width < canvas.height ? canvas.width * 0.7 : canvas.height * 0.7;
+        this.lineColor = "black";
+        this.lineWidth = this.insideRadius > 150 ? 2 : 1;
+        this.segmentColor1 = "#FFFF00";
+        this.segmentColor2 = "#FF0000";
+
+        this.segmentAngles = [12.857142, 38.571428, 38.571428, 38.571428,
+            38.571428, 38.571428, 38.571428, 38.571428, 38.571428, 38.571428];
+        this.segmentNames = ["K", "B1", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "B2"];
+        this.cylinder = {count: 28, word: "U", start: 1, step: 1, firstAngle: 0};
+    }
+
+    setRingRose(x, y, radius) {
+        this.location = {
+            x: x,
+            y: y
+        };
+        this.insideRadius = radius;
+    }
+
+    setNameFont(fontWeight, fontSize, fontFamily, color) {
+        this.context.font = fontWeight + " " + fontSize + "px " + fontFamily;
+        this.context.textAlign = "center";
+        this.context.textBaseline = "middle";
+        this.context.strokeStyle = color;
+        this.context.fillStyle = color
+    }
+
+    draw(color, fill) {
+        this.context.lineWidth = this.lineWidth;
+        this.context.strokeStyle = color;
+        this.context.fillStyle = color
+        fill ? this.context.fill() : this.context.stroke();
+    }
+
+    drawSegment(radius, crossAngle, color, fill) {
+        crossAngle = Rad(crossAngle);
+        this.context.beginPath();
+        this.context.arc(0, 0, radius, 0, crossAngle, false);
+        this.context.arc(0, 0, radius + this.ringWidth, crossAngle, 0, true);
+        this.draw(color, fill);
+    }
+
+    drawRingBackground(radius) {
+        this.context.save();
+        let angle = 0, colorNumber = 0, colorArray = [this.segmentColor1, this.segmentColor2];
+        for (let i = 0; i < this.segmentAngles.length; i++) {
+            this.context.rotate(Rad(angle));
+            colorNumber = colorNumber === 0 ? 1 : 0;
+            colorNumber = this.segmentAngles.length % 2 === 0 ? colorNumber : 1;
+            this.drawSegment(radius, this.segmentAngles[i], colorArray[colorNumber], true);
+            angle = this.segmentAngles[i];
+        }
+        this.context.restore();
+    }
+
+    drawRingLine(radius) {
+        this.context.save();
+        let angle = 0;
+        for (let i = 0; i < this.segmentAngles.length; i++) {
+            this.context.rotate(Rad(angle));
+            this.drawSegment(radius, this.segmentAngles[i], this.lineColor, false);
+            angle = this.segmentAngles[i];
+        }
+        this.context.restore();
+    }
+
+    drawSegmentNames(radius) {
+        let angle = this.segmentAngles[0] / 2 + 90;
+        this.setNameFont("bolder", 15, "Arial", "black");
+        this.context.save();
+        for (let i = 0; i < this.segmentNames.length; i++) {
+            if (i !== 0) {
+                angle += this.segmentAngles[i] / 2;
+            }
+            this.context.rotate(Rad(angle));
+            this.context.fillText(this.segmentNames[i], 0, -(radius + this.ringWidth / 2));
+            angle = this.segmentAngles[i] / 2;
+        }
+        this.context.restore();
+
+    }
+
+    drawRing(cylinderNumber, isRingIn) {
+        let radius = this.insideRadius;
+        if (!isRingIn) radius = this.insideRadius + this.ringWidth * 1.1;
+        let angle = (360 / this.cylinder.count) * cylinderNumber + this.cylinder.firstAngle;
+        this.context.save();
+        this.context.translate(this.location.x, this.location.y);
+        this.context.rotate(-(Math.PI / 2 + Rad(this.segmentAngles[0] / 2)) + Rad(angle));
+        this.drawRingBackground(radius);
+        this.drawRingLine(radius);
+        this.drawSegmentNames(radius);
+        this.context.restore();
+    }
+
+    drawCylinders(isDrawNames) {
+        // cylinder
+        this.context.save();
+        this.context.translate(this.location.x, this.location.y);
+        let radius = this.insideRadius + this.ringWidth * 2.6;
+        this.context.beginPath();
+        this.context.arc(0, 0, radius, 0, 2 * Math.PI);
+        this.draw(this.lineColor, false);
+
+        for (let i = 0; i < this.cylinder.count; i++) {
+            this.context.rotate(Rad(360 / this.cylinder.count));
+            this.context.beginPath();
+            this.context.arc(0, -radius, 3, 0, 2 * Math.PI);
+            this.draw("red", true);
+            this.draw(this.lineColor, false);
+        }
+
+        // cylinder names
+        if (!isDrawNames) return;
+        radius = this.insideRadius + this.ringWidth * 2.6;
+        for (let i = 0; i < this.cylinder.count; i++) {
+            this.context.beginPath();
+            this.setNameFont("bolder", this.ringWidth / 3, "Arial", this.lineColor);
+            let number = this.cylinder.start + this.cylinder.step * i;
+            this.context.fillText(this.cylinder.word + number, 0, -radius - this.ringWidth / 2);
+            this.context.rotate(Rad(360 / this.cylinder.count));
+        }
+        this.context.restore();
+    }
+
+    getInitialGapPosition(){
+        let gap = [];
+        for (let i=0;i<this.segmentAngles.length;i++){
+            if (i === 0){
+                gap.push(this.segmentAngles[i] / 2);
+            }
+            else{
+                let angle = gap[i-1] + this.segmentAngles[i];
+                gap.push(angle);
+            }
+        }
+        return gap;
+    }
+
+    getGapPosition(angle){
+        let gap = this.getInitialGapPosition();
+        for (let i=0;i<gap.length;i++){
+            gap[i] = gap[i] + angle < 360 ? gap[i] + angle : gap[i] + angle - 360;
+        }
+        return gap;
+    }
+
+    checkAngleInArray(angle, angleArray, offset){
+        for (let i = 0;i<angleArray.length;i++){
+            if (Math.abs(angle - angleArray[i]) < offset){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    checkGap(ringOut, ringIn, isDraw) {
+        let sameGapAngle = false;
+        let cylinderAngle = 360 / this.cylinder.count;
+        let outArray = this.getGapPosition(ringOut * cylinderAngle);
+        let inArray = this.getGapPosition(ringIn * cylinderAngle);
+        for (let i = 0;i< outArray.length; i++){
+            let check = this.checkAngleInArray(outArray[i], inArray, 3);
+            if (check){
+                sameGapAngle = true;
+                if (isDraw) {
+                    this.context.save();
+                    this.context.translate(this.location.x, this.location.y);
+                    this.context.rotate(Rad(outArray[i] - 91));
+                    this.drawSegment(this.insideRadius * 0.7, 2, "#06266F", true);
+                    this.drawSegment(this.insideRadius * 1.15, 2, "#1240AB", true);
+                    this.drawSegment(this.insideRadius * 1.6, 2, "#6C8CD5", true);
+                    this.context.restore();
+                }
+            }
+        }
+        return sameGapAngle;
+    }
+}
---
+++ RingRose/js/form.js
@@ -0,0 +1,295 @@
+/**
+ * Draw setting card tab change.
+ * @param element
+ */
+function tabChange(element) {
+    let tabs = ["segmentTab", "cylinderTab", "rotateTab"],
+        panels = ["segmentSetting", "cylinderSetting", "rotateSetting"];
+    let index = tabs.indexOf(element.id);
+    let drawButton = document.querySelector("#drawButton");
+    if (index !== -1) {
+        drawButton.className = index === 0 || index === 1 ? "btn btn-primary" : "d-none btn btn-primary";
+        for (let i = 0; i < tabs.length; i++) {
+            let tab = document.querySelector("#" + tabs[i]);
+            let panel = document.querySelector("#" + panels[i]);
+            if (i === index) {
+                tab.className = "nav-link active";
+                panel.className = "card-body";
+                if (i !== 2) {
+                    drawRingRose();
+                    rose.checkGap(ringOutsideNumber, ringInsideNumber, false);
+                }else if (i === 2){
+                    drawRingRose();
+                    rose.checkGap(ringOutsideNumber, ringInsideNumber, true);
+                }
+            } else {
+                tab.className = "nav-link";
+                panel.className = "d-none card-body";
+            }
+        }
+    }
+}
+
+/**
+ * Create input element for segment names and angles form.
+ * @param parent
+ * @param className
+ * @param id {string}
+ * @param type {string}
+ * @param value
+ */
+function createInput(parent, className, id, type, value) {
+    let input = document.createElement("input");
+    input.id = id;
+    input.className = className;
+    input.type = type;
+    input.value = value
+    parent.append(input)
+}
+
+function createSpan(parent, string) {
+    let span = document.createElement("span");
+    span.className = "input-group-text";
+    span.innerHTML = string;
+    parent.append(span);
+}
+
+/**
+ * Create all segment names and angles
+ * @param segmentNames {[]}
+ * @param segmentAngles {[]}
+ */
+function createSegmentInfoInput(segmentNames, segmentAngles) {
+    let segmentInfo = document.querySelector("#segmentInfo");
+    segmentInfo.innerHTML = "";
+    for (let i = 0; i < segmentAngles.length; i++) {
+        let div = document.createElement("div");
+        div.className = "input-group input-group-sm";
+        let number = i + 1 < 10 ? "0" + (i + 1) : i + 1;
+        createSpan(div, number + ". ");
+        createInput(div, "form-control text-center", "segmentName" + i, "text", segmentNames[i]);
+        createInput(div, "form-control text-end", "segmentAngle" + i, "number", segmentAngles[i]);
+        createSpan(div, "°");
+        segmentInfo.append(div);
+    }
+}
+
+/**
+ * Change segment count from input number.
+ */
+function changeSegmentCount() {
+    let count = document.querySelector("#segmentCount").value;
+    if (count > 0) {
+        let segmentNames = [];
+        for (let i = 0; i < count; i++) {
+            if (i === 0) {
+                segmentNames.push("K");
+            } else if (i === 1) {
+                segmentNames.push("B1");
+            } else if (i === (count - 1)) {
+                segmentNames.push("B2")
+            } else {
+                segmentNames.push("A" + (i - 1));
+            }
+        }
+        let segmentAngles = [];
+        for (let i = 0; i < segmentNames.length; i++) {
+            //angles.push("");
+            segmentAngles.push(360 / count);
+        }
+        createSegmentInfoInput(segmentNames, segmentAngles);
+    }
+}
+
+/**
+ * Show error prompt.
+ * @param error {string} error message.
+ */
+function errorPrompt(error) {
+    const modal = new bootstrap.Modal('#infoModal', {
+        keyboard: false
+    });
+    const modalContent = document.querySelector("#modalBody");
+    const modalTitle = document.querySelector("#modalTitle");
+    modalTitle.innerHTML = "报错提示：";
+    modalContent.innerHTML = error;
+    modal.show();
+}
+
+let mainCanvas = document.querySelector("#canvas");
+let rose = new RingRoseDrawer(mainCanvas);
+let ringInsideNumber = 0, ringOutsideNumber = 0, drawSameGap = true;
+
+/**
+ * Get ring information from input form.
+ */
+function getDrawInformation() {
+    // SEGMENT
+    let segmentCount = parseInt(document.querySelector("#segmentCount").value);
+    let segmentInfo = document.querySelector("#segmentInfo");
+    if (segmentCount < 1) {
+        errorPrompt("请检查管片数量输入框！");
+    } else {
+        if (segmentCount !== segmentInfo.children.length) {
+            errorPrompt("请按下管片数量变更设置按钮，并重新填写名称和角度！");
+            return;
+        }
+        let segmentNames = [], segmentAngles = [];
+        for (let i = 0; i < segmentCount; i++) {
+            let temp = document.querySelector("#segmentName" + i).value;
+            if (temp === "") {
+                errorPrompt("未查询到 {管片 - " + (i + 1) + "} 的命名！请检查输入的信息！");
+                return;
+            }
+            segmentNames.push(temp);
+            temp = document.querySelector("#segmentAngle" + i).value;
+            if (temp === "") {
+                errorPrompt("输入的管片角度为空 ！");
+                return;
+            }
+            temp = parseFloat(temp);
+            if (temp < 0) {
+                errorPrompt("输入的管片角度应大于等于 0 ！");
+                return;
+            }
+            segmentAngles.push(temp);
+        }
+        rose.segmentNames = segmentNames;
+        rose.segmentAngles = segmentAngles;
+    }
+    // CYLINDER
+    let cylinderCount = document.querySelector("#cylinderCount").value;
+    if (cylinderCount === "") {
+        errorPrompt("输入的油缸数量为空！");
+        return;
+    }else if (parseInt(cylinderCount) < 2) {
+        errorPrompt("输入的油缸数量应大于等于 2 ！");
+        return;
+    }else {
+        rose.cylinder.count = parseInt(cylinderCount);
+    }
+    let cylinderAngle = document.querySelector("#cylinderAngleStart").value;
+    if (cylinderAngle === "") {
+        errorPrompt("油缸起始角度输入为空！！");
+        return;
+    } else {
+        rose.cylinder.firstAngle = parseFloat(cylinderAngle);
+    }
+    let cylinderStart = document.querySelector("#cylinderNumberStart").value;
+    if (cylinderStart === "") {
+        errorPrompt("油缸起始编号输入为空！");
+        return;
+    } else {
+        rose.cylinder.start = parseInt(cylinderStart);
+    }
+    let cylinderStep = document.querySelector("#cylinderNumberStep").value;
+    if (cylinderStep === "") {
+        errorPrompt("增加步长输入为空！！");
+        return;
+    } else {
+        rose.cylinder.step = parseInt(cylinderStep);
+    }
+    rose.cylinder.word = document.querySelector("#cylinderWord").value;
+}
+
+function setCanvasSize() {
+    let screen = document.body.getBoundingClientRect();
+    let canvasContainer = document.querySelector("#canvasContainer").getBoundingClientRect();
+
+    let side = canvasContainer.width;
+    mainCanvas.width = side;
+    side > screen.height ? mainCanvas.height = screen.height : mainCanvas.height = side;
+}
+
+function drawRingRose() {
+    rose.context.clearRect(0, 0, rose.canvas.width, rose.canvas.height);
+    rose.location.x = mainCanvas.width / 2;
+    rose.location.y = mainCanvas.height / 2;
+    rose.drawRing(ringInsideNumber, true);
+    rose.drawRing(ringOutsideNumber, false);
+    rose.drawCylinders(true);
+}
+
+function rotateRing(isIn, isClockwise) {
+    let cylinderNumber = isIn ? ringInsideNumber : ringOutsideNumber;
+    if (isClockwise)
+        cylinderNumber = cylinderNumber + 1 > rose.cylinder.count - 1 ? 0 : cylinderNumber + 1;
+    else
+        cylinderNumber = cylinderNumber - 1 < 0 ? rose.cylinder.count - 1 : cylinderNumber - 1;
+    isIn ? ringInsideNumber = cylinderNumber : ringOutsideNumber = cylinderNumber;
+    document.querySelector("#ringInCylinder").innerHTML = rose.cylinder.word + (ringInsideNumber + 1);
+    document.querySelector("#ringOutCylinder").innerHTML = rose.cylinder.word + (ringOutsideNumber + 1);
+    drawRingRose();
+    document.querySelector("#gapCheck");
+    rose.checkGap(ringOutsideNumber, ringInsideNumber, drawSameGap);
+}
+
+function createGapTable() {
+    let table = document.querySelector("#gapTable");
+    table.innerHTML = "";
+    for (let i = 0; i < rose.cylinder.count; i++) {
+        let tr = document.createElement("tr");
+        table.append(tr);
+        let th = document.createElement("th");
+        th.setAttribute("scope", "row");
+        th.innerText = rose.cylinder.word + (i + 1) + ":";
+        tr.append(th);
+        for (let j = 0; j < rose.cylinder.count; j++) {
+            if (!rose.checkGap(i, j, false)) {
+                let td = document.createElement("td");
+                td.innerText = rose.cylinder.word + (j + 1);
+                tr.append(td);
+            }
+        }
+    }
+}
+
+function showSameGap(element) {
+    drawSameGap = !!element.checked;
+    drawRingRose();
+    rose.checkGap(ringOutsideNumber, ringInsideNumber, drawSameGap);
+}
+
+/**
+ * Draw button click event.
+ */
+function drawButtonClick(){
+    ringInsideNumber = ringOutsideNumber = 0;
+    getDrawInformation();
+    drawRingRose();
+    createGapTable();
+}
+
+function init() {
+
+    const modal = new bootstrap.Modal('#welcomeModal', {
+        keyboard: false
+    });
+    modal.show();
+
+    document.querySelector("#segmentCount").value = 10;
+    document.querySelector("#cylinderCount").value = 28;
+    document.querySelector("#cylinderWord").value = "U";
+    document.querySelector("#cylinderNumberStart").value = 1;
+    document.querySelector("#cylinderNumberStep").value = 1;
+    document.querySelector("#cylinderAngleStart").value = 0;
+
+    let segmentNames = ["K", "B1", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "B2"];
+    let segmentAngles = [12.857142, 38.571428, 38.571428, 38.571428,
+        38.571428, 38.571428, 38.571428, 38.571428, 38.571428, 38.571428];
+
+    createSegmentInfoInput(segmentNames, segmentAngles);
+    setCanvasSize();
+    drawRingRose();
+
+    let size = document.querySelector("#segmentSetting").getBoundingClientRect();
+    let table = document.querySelector("#tableContainer");
+    table.setAttribute("style", "height:" + size.height + "px");
+    createGapTable();
+}
+
+document.body.onload = init;
+document.body.onresize = function () {
+    setCanvasSize();
+    drawRingRose();
+};
