6d035c00da6471ca9f344cc76e33f3d1f3c46454
Muziyi <None@none.none>
2022-09-08 22:46:59 +0800

    更新 css3d 项目

--- js/listItem.js
+++ js/listItem.js
@@ -10,5 +10,11 @@ const items = [
         title: "纯 CSS3 创建 3D 图形",
         time: "2022.09.05",
         describe: "基于 CSS3 的三维变换功能创建简单的 3D 图形。"
+    },
+    {
+        href: "Pages/CSSEffects01/index.html",
+        title: "一些使用 CSS 制作的简单效果",
+        time: "2022.09.08",
+        describe: "基于 box-shadow 和 text-shadow 制作的简单特效。"
     }
 ];
