1aec93e61e9504299a0a64821967688f0ffee46e
a <a>
2022-09-09 17:20:53 +0800

    中秋快乐！

--- Pages/MidAutumn/script.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector(".container");
-const screen = container.getBoundingClientRect();
-const card = document.querySelector(".card");
-document.body.onmousemove = function (event){
-    let ox = event.clientX;
-    let oy = event.clientY;
-    let angle = 30 * (ox - screen.width / 2) / (screen.width / 2);
-    card.style.transform = "rotateY(" + angle + "deg)";
-}
